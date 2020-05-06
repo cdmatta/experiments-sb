3c6bd490dd288b42eb8df9a69800b2f8e9b2abe7
Charandeep S. Matta <charandeep.matta@gmail.com>
2020-05-07 00:39:42 +0300

    init

---
+++ src/test/java/com/github/cdmatta/experiment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cdmatta.experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
