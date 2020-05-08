4cbadbc28a21b4eee08f8a063705a6ae02c06b2c
Charandeep S. Matta <charandeep.matta@gmail.com>
2020-05-09 02:52:05 +0300

    dpkg with vuejs

--- projects/dpkg/src/main/resources/public/js/app.js
+++ projects/dpkg/src/main/resources/public/js/app.js
@@ -1,23 +1,34 @@
-var dpkgStatus = angular.module('dpkgStatus', ['ngResource']);
-
-dpkgStatus.controller('DpkgResourceCtrl', [ '$scope', 'DpkgResourceService', function($scope, DpkgResourceService) {
-	$scope.searchString = '';
-	$scope.selectedPackage = null;
-
-    $scope.updatePackageSelection = function(newPackage) {
-    	$scope.selectedPackage = $scope.dpkg[newPackage];
-    };
-
-	DpkgResourceService.all.query().$promise.then(function (result) {
-		$scope.dpkg = JSON.parse(angular.toJson(result));
-		$scope.packages = _.values($scope.dpkg);
-	});
-}]);
-
-dpkgStatus.factory('DpkgResourceService', ['$resource', function($resource) {
-	return {
-		all: $resource('/all', {}, {
-			query: {method: 'GET',  params: {}, isArray: false}
-		}),
-	};
-}]);
+new Vue({
+    el: "#app",
+    data: {
+        searchString: "",
+        selectedPackage: null,
+        dpkg: null,
+        packages: null,
+        packagesDisplayList: null
+    },
+    mounted() {
+        axios
+            .get('/all')
+            .then(response => {
+                this.dpkg = response.data;
+                this.packages = _.values(this.dpkg);
+                this.packagesListToShow = _.values(this.dpkg);
+                this.updateDisplayPackages(this.searchString);
+                return;
+            })
+    },
+    watch: {
+        searchString: function (val) {
+            this.updateDisplayPackages(val);
+        }
+    },
+    methods: {
+        updateDisplayPackages(search) {
+            this.packagesDisplayList = _.sortBy(_.filter(this.packages, function (p) { return p.name.includes(search) }), [function (p) { return p.name; }]);
+        },
+        updatePackageSelection(name) {
+            this.selectedPackage = this.dpkg[name];
+        }
+    }
+});
